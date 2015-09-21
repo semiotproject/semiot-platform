90cfad3ba915d4ff09ab6b817980a413d4a9c4e2
Ivan Shilin <proniment@gmail.com>
2015-09-21 15:44:04 +0300

    Update run.sh
    Fixed run order.

--- device-directory-service/run.sh
+++ device-directory-service/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+java -jar target/$SERVICE_JAR_NAME.jar &> devicedirectory.log &
+
 pushd $FUSEKI_HOME
 
-./fuseki-server --config=base/config.ttl &> fuseki.log &
+./fuseki-server --config=base/config.ttl
 
 popd
-
-java -jar target/$SERVICE_JAR_NAME.jar
