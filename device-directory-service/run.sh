939e1ab77e33b3c90a19549d40a45c4a93a41931
Ivan Shilin <proniment@gmail.com>
2015-09-24 12:11:23 +0300

    Replace run.sh to old version.

--- device-directory-service/run.sh
+++ device-directory-service/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-java -jar target/$SERVICE_JAR_NAME.jar &> devicedirectory.log &
-
 pushd $FUSEKI_HOME
 
-./fuseki-server --config=base/config.ttl
+./fuseki-server --config=base/config.ttl &> fuseki.log &
 
 popd
+
+java -jar target/$SERVICE_JAR_NAME.jar
