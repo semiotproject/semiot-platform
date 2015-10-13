99d4d088e0e098aac821792784109e5a00fe3f96
soylent-grin <hocico16@gmail.com>
2015-10-13 21:17:04 +0300

    Removed mock systems initialization

--- web-ui/src/js/models/system-list.js
+++ web-ui/src/js/models/system-list.js
@@ -31,7 +31,7 @@ export default function(
                 ]
             ).session;
 
-            this.launchTestSystems();
+            // this.launchTestSystems();
         }
 
         launchTestSystems() {
