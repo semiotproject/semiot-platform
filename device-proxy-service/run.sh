6c2570fcc18596d0a1755e5af5adef2889d7f4e6
Maxim Kolchin <kolchinmax@gmail.com>
2015-10-28 12:20:45 +0300

    #54 Refactored Device Proxy Service Dockerfile

--- device-proxy-service/run.sh
+++ device-proxy-service/run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 while ! curl -Is http://devicedirectoryservice:3030
 do
-  sleep 1;
+    echo "$(date) - Failed to connect to Fuseki! Trying once more time..."   
+    sleep 5;
 done
-echo "$(date) - connected successfully"
+echo "$(date) - Connected to Fuseki successfully"
 
 java -jar device-proxy-service-launcher/target/$SERVICE_JAR_NAME.jar
