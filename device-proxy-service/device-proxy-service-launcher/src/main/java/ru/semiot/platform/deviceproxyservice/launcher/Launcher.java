ffb1b1a3a320e48fdf6f8f41d9b84d314cf21863
Amberdr0p <proniment@gmail.com>
2015-10-30 14:51:52 +0300

    Added ShutdownHook for Launcher. Close #62.

--- device-proxy-service/device-proxy-service-launcher/src/main/java/ru/semiot/platform/deviceproxyservice/launcher/Launcher.java
+++ device-proxy-service/device-proxy-service-launcher/src/main/java/ru/semiot/platform/deviceproxyservice/launcher/Launcher.java
@@ -13,63 +13,92 @@ import org.osgi.framework.launch.FrameworkFactory;
 import org.osgi.framework.Bundle;
 
 public class Launcher {
-
+	private static Framework framework = null;
+	
     public static void main(String args[]) throws BundleException {
-
+    	
         ServiceLoader<FrameworkFactory> loader = ServiceLoader.load(FrameworkFactory.class);
         FrameworkFactory frameworkFactory = loader.iterator().next();
 
         Map<String, String> config = new HashMap<>();
         config.put("org.osgi.service.http.port", "8181");
-
-        Framework framework = frameworkFactory.newFramework(config);
-        framework.start();
-
-        BundleContext context = framework.getBundleContext();
-
-        List<Bundle> bundles = new ArrayList<>();
-
-        //Apache Felix bundles
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.configadmin-1.8.8.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.log-1.0.1.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.metatype-1.1.2.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.osgi.compendium-1.4.0.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.webconsole-4.2.12-all.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.http.servlet-api-1.1.2.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.http.api-3.0.0.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.http.jetty-3.1.0.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.eventadmin-1.4.4.jar?raw=true"));
-        bundles.add(context.installBundle(
-                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.dependencymanager-4.1.0.jar?raw=true"));
-
-        bundles.add(context.installBundle("http://central.maven.org/maven2/com/github/livesense/org.liveSense.fragment.sun.misc/1.0.5/org.liveSense.fragment.sun.misc-1.0.5.jar"));
         
-        //Device Proxy Service bundles
-        bundles.add(context.installBundle(
-                "device-proxy-service-api-1.0-SNAPSHOT.jar",
-                Launcher.class.getResourceAsStream("/bundles/device-proxy-service-api-1.0-SNAPSHOT.jar")));
-        bundles.add(context.installBundle(
-                "device-proxy-service-manager-1.0-SNAPSHOT.jar",
-                Launcher.class.getResourceAsStream("/bundles/device-proxy-service-manager-1.0-SNAPSHOT.jar")));
+        Runtime.getRuntime().addShutdownHook(new Thread("Felix Shutdown Hook") {
+            public void run()
+            {
+                try
+                {
+                    if (framework != null)
+                    {
+                    	System.out.println("Framework stoping");
+                    	framework.stop();
+                    	framework.waitForStop(0);
+                    	System.out.println("Framework stoped");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.err.println("Error stopping framework: " + ex);
+                }
+            }
+        });
 
-        System.out.println("Installed all bundles!");
-
-        System.out.println("Starting all of them...");
-        for (Bundle bundle : bundles) {
-            bundle.start();
-
-            System.out.format("Started [%s:%s]. Current state: %s\n",
-                    bundle.getSymbolicName(), bundle.getVersion(), bundle.getState());
-        }
+        try {
+	        framework = frameworkFactory.newFramework(config);
+	        // framework.init();
+	        framework.start();
+	        
+	        BundleContext context = framework.getBundleContext();
+	
+	        List<Bundle> bundles = new ArrayList<>();
+	
+	        //Apache Felix bundles
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.configadmin-1.8.8.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.log-1.0.1.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.metatype-1.1.2.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.osgi.compendium-1.4.0.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.webconsole-4.2.12-all.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.http.servlet-api-1.1.2.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.http.api-3.0.0.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.http.jetty-3.1.0.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.eventadmin-1.4.4.jar?raw=true"));
+	        bundles.add(context.installBundle(
+	                "https://github.com/semiotproject/semiot-platform/blob/bundles/felix-bundles/org.apache.felix.dependencymanager-4.1.0.jar?raw=true"));
+	        bundles.add(context.installBundle("http://central.maven.org/maven2/com/github/livesense/org.liveSense.fragment.sun.misc/1.0.5/org.liveSense.fragment.sun.misc-1.0.5.jar"));
+	       
+	        //Device Proxy Service bundles
+	        bundles.add(context.installBundle(
+	                "device-proxy-service-api-1.0-SNAPSHOT.jar",
+	                Launcher.class.getResourceAsStream("/bundles/device-proxy-service-api-1.0-SNAPSHOT.jar")));
+	        bundles.add(context.installBundle(
+	                "device-proxy-service-manager-1.0-SNAPSHOT.jar",
+	                Launcher.class.getResourceAsStream("/bundles/device-proxy-service-manager-1.0-SNAPSHOT.jar")));
+	
+	        System.out.println("Installed all bundles!");
+	
+	        System.out.println("Starting all of them...");
+	        for (Bundle bundle : bundles) {
+	            bundle.start();
+	
+	            System.out.format("Started [%s:%s]. Current state: %s\n",
+	                    bundle.getSymbolicName(), bundle.getVersion(), bundle.getState());
+	        }
+	        
+			framework.waitForStop(0);
+			System.exit(0);
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+	        System.exit(0);
+		}
     }
 
 }
