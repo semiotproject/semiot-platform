848ff453887d90b6747a37863ccc907f44db5a43
Amberdr0p <proniment@gmail.com>
2016-02-25 16:48:35 +0300

    Update opentsdb version to 2.2.0.

--- docker/opentsdb/run.sh
+++ docker/opentsdb/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 mkdir -p /etc/opentsdb/ && touch /etc/opentsdb/opentsdb.conf && \
-echo "tsd.http.request.cors_domains=*\ntsd.core.meta.enable_realtime_ts=True\ntsd.core.auto_create_metrics=True" > /etc/opentsdb/opentsdb.conf
+echo "tsd.http.request.cors_domains=*\ntsd.core.meta.enable_realtime_ts=True\ntsd.core.auto_create_metrics=True\ntsd.http.query.allow_delete=True" > /etc/opentsdb/opentsdb.conf
 # Bad bad practice
 #Wait until hbase creates tables
 flag=$(curl -s hbase:60010/table.jsp?name=tsdb | grep "Region Server" )
 while [ -z "$flag" ]; do
 	sleep 5
 	echo "Sleep until hbase is creating tables"
-	flag=$(curl -s hbase:60010/table.jsp?name=tsdb | grep "Region Server" )	
+	flag=$(curl -s hbase:60010/table.jsp?name=tsdb | grep "Region Server" )
 done
 echo "Tables created"
 echo "Start OpenTSDB"
