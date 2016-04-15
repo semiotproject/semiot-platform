e8226356a1683c86a9c5dcfad43d2e5bfb6beaff
Ivan <proniment@gmail.com>
2016-04-15 14:34:13 +0300

    Fixed run.sh

--- device-proxy-service/run.sh
+++ device-proxy-service/run.sh
@@ -17,7 +17,7 @@ popd
 
 while ! curl -Is http://localhost:3030
 do
-    echo "$(date) - Failed to connect to Fuseki! Trying once more time..."   
+    echo "$(date) - Failed to connect to Fuseki! Trying once more time..."
     sleep 5;
 done
 echo "$(date) - Connected to Fuseki successfully"
