2070ec30dfa6779b29895b00e08c20a2aa0c81be
Maxim Kolchin <kolchinmax@gmail.com>
2016-04-18 11:41:08 +0300

    Refactored URL paths to static content

--- api-gateway/src/main/websrc/src/js/explorer/routing.js
+++ api-gateway/src/main/websrc/src/js/explorer/routing.js
@@ -6,27 +6,27 @@ export default function($routeProvider) {
             redirectTo: '/systems'
         })
         .when('/login', {
-            templateUrl: '/static/partials/login.html',
+            templateUrl: '/partials/login.html',
             controller: 'LoginCtrl'
         })
         .when('/systems', {
-            templateUrl: '/static/partials/system-list.html',
+            templateUrl: '/partials/system-list.html',
             controller: 'SystemListCtrl'
         })
         .when('/systems/:system_uri*', {
-            templateUrl: '/static/partials/system-detail.html',
+            templateUrl: '/partials/system-detail.html',
             controller: 'SystemDetailCtrl'
         })
         .when('/queries', {
-            templateUrl: '/static/partials/query-list.html',
+            templateUrl: '/partials/query-list.html',
             controller: 'QueryListCtrl'
         })
         .when('/queries/:query_id', {
-            templateUrl: '/static/partials/query-detail.html',
+            templateUrl: '/partials/query-detail.html',
             controller: 'QueryDetailCtrl'
         })
         .when('/new-query', {
-            templateUrl: '/static/partials/new-query.html',
+            templateUrl: '/partials/new-query.html',
             controller: 'NewQueryCtrl'
         });
 }
