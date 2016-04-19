d384ae8791828754115b65cc7798f9235103bed6
Maxim Kolchin <kolchinmax@gmail.com>
2016-04-19 10:02:43 +0300

    #163 Added debug logs in UploadDriverHandler

--- api-gateway/src/main/java/ru/semiot/platform/apigateway/config/UploadDriverHandler.java
+++ api-gateway/src/main/java/ru/semiot/platform/apigateway/config/UploadDriverHandler.java
@@ -1,5 +1,9 @@
 package ru.semiot.platform.apigateway.config;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -14,12 +18,23 @@ import javax.servlet.http.Part;
 @WebServlet(urlPatterns = "/UploadDriverHandler", asyncSupported = true)
 @MultipartConfig
 public class UploadDriverHandler extends HttpServlet {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(UploadDriverHandler.class);
+
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
 
     	Part part = request.getPart("bundlefile");
     	HttpSession session = request.getSession(true);
+
+		for(Part p : request.getParts()) {
+			logger.debug("Part name: {}", p.getName());
+			logger.debug("Part [{}] fileName: {}", p.getName(),
+					p.getSubmittedFileName());
+			logger.debug("Part [{}] size: {}", p.getName(), p.getSize());
+			logger.debug("Part [{}] inputstream: {}", p.getName(),
+					IOUtils.toString(p.getInputStream()));
+		}
     	
     	session.setAttribute("inputStreamFile", part.getInputStream());
     	session.setAttribute("filename", part.getSubmittedFileName());
