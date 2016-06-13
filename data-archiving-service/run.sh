4aa06c67a138cc7f7850bbe2162ffef4f75e76b8
Maxim Kolchin <kolchinmax@gmail.com>
2016-06-13 20:46:37 +0300

    Configured JVM based parts for performance monitoring

--- data-archiving-service/run.sh
+++ data-archiving-service/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar $SERVICE_JAR_NAME.jar
+java -javaagent:inspectit/agent/inspectit-agent.jar -Dinspectit.config=inspectit/agent/config/ -Xbootclasspath/p:inspectit/agent/inspectit-agent.jar -jar $SERVICE_JAR_NAME.jar
