54a7427c6a22eef470ecb591f63d44b94f1a6108
Maxim Kolchin <kolchinmax@gmail.com>
2016-06-14 20:23:50 +0300

    Switched to new version 1.6.8.82 of inspectIT

--- data-archiving-service/run.sh
+++ data-archiving-service/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -javaagent:inspectit/agent/inspectit-agent.jar -Dinspectit.config=inspectit/agent/config/ -Xbootclasspath/p:inspectit/agent/inspectit-agent.jar -jar $SERVICE_JAR_NAME.jar
+java -javaagent:inspectit/agent/inspectit-agent.jar -Dinspectit.repository=winghouse.semiot.ru:9070 -Dinspectit.agent.name=tsdbservice -Xbootclasspath/p:inspectit/agent/inspectit-agent.jar -jar $SERVICE_JAR_NAME.jar
