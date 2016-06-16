1452a16951745b4b3bd1efcd31eaeac50d5971c6
soylent-grin <hocico16@gmail.com>
2016-06-16 14:18:15 +0300

    #184 work in progress

--- api-gateway/src/main/websrc/src/js/systems/api/processes.js
+++ api-gateway/src/main/websrc/src/js/systems/api/processes.js
@@ -2,9 +2,9 @@ export default ["CONFIG", "HTTP", "WAMP", "$q", "commonUtils", function(CONFIG, 
 
     Object.prototype.renameProperty = function (oldName, newName) {
          // Do nothing if the names are the same
-         if (oldName == newName) {
-             return this;
-         }
+        if (oldName == newName) {
+            return this;
+        }
         // Check for the old property name to avoid a ReferenceError in strict mode.
         if (this.hasOwnProperty(oldName)) {
             this[newName] = this[oldName];
