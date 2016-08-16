05a2db0fbd8491bd93d9effef84723e14133ebcd
Amberdr0p <proniment@gmail.com>
2016-08-16 12:23:12 +0400

    Changed pid for manager.

--- device-proxy-service/device-manager/src/main/java/ru/semiot/platform/deviceproxyservice/manager/Keys.java
+++ device-proxy-service/device-manager/src/main/java/ru/semiot/platform/deviceproxyservice/manager/Keys.java
@@ -2,17 +2,17 @@ package ru.semiot.platform.deviceproxyservice.manager;
 
 public class Keys extends ru.semiot.platform.deviceproxyservice.api.drivers.Keys {
 
-  public static final String TOPIC_NEWANDOBSERVING = "ru.semiot.platform.deviceproxyservice.manager.topic_newandobserving";
-  public static final String TOPIC_INACTIVE = "ru.semiot.platform.deviceproxyservice.manager.topic_inactive";
-  public static final String WAMP_URI = "ru.semiot.platform.deviceproxyservice.manager.wamp_uri";
-  public static final String WAMP_REALM = "ru.semiot.platform.deviceproxyservice.manager.wamp_realm";
-  public static final String WAMP_RECONNECT = "ru.semiot.platform.deviceproxyservice.manager.wamp_reconnect";
+  public static final String TOPIC_NEWANDOBSERVING = "ru.semiot.platform.device-proxy-service-manager.topic_newandobserving";
+  public static final String TOPIC_INACTIVE = "ru.semiot.platform.device-proxy-service-manager.topic_inactive";
+  public static final String WAMP_URI = "ru.semiot.platform.device-proxy-service-manager.wamp_uri";
+  public static final String WAMP_REALM = "ru.semiot.platform.device-proxy-service-manager.wamp_realm";
+  public static final String WAMP_RECONNECT = "ru.semiot.platform.device-proxy-service-manager.wamp_reconnect";
   public static final String WAMP_LOGIN = "ru.semiot.platform.wamp_login";
   public static final String WAMP_PASSWORD = "ru.semiot.platform.wamp_password";
-  public static final String FUSEKI_PASSWORD = "ru.semiot.platform.deviceproxyservice.manager.fuseki_pass";
-  public static final String FUSEKI_USERNAME = "ru.semiot.platform.deviceproxyservice.manager.fuseki_username";
-  public static final String FUSEKI_UPDATE_URL = "ru.semiot.platform.deviceproxyservice.manager.fuseki_update_url";
-  public static final String FUSEKI_QUERY_URL = "ru.semiot.platform.deviceproxyservice.manager.fuseki_query_url";
-  public static final String FUSEKI_STORE_URL = "ru.semiot.platform.deviceproxyservice.manager.fuseki_store_url";
+  public static final String FUSEKI_PASSWORD = "ru.semiot.platform.device-proxy-service-manager.fuseki_pass";
+  public static final String FUSEKI_USERNAME = "ru.semiot.platform.device-proxy-service-manager.fuseki_username";
+  public static final String FUSEKI_UPDATE_URL = "ru.semiot.platform.device-proxy-service-manager.fuseki_update_url";
+  public static final String FUSEKI_QUERY_URL = "ru.semiot.platform.device-proxy-service-manager.fuseki_query_url";
+  public static final String FUSEKI_STORE_URL = "ru.semiot.platform.device-proxy-service-manager.fuseki_store_url";
 
 }
